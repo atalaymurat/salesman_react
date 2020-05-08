973bc594d2a093bda3a81c3de0bbf7bf2f9e4c3a
mAta <atalaymurat@msn.com>
2020-05-09 00:52:06 +0300

    React Router Navbar Setup

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <Link className="navbar-brand" to="/">MakinaTR</Link>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="nav navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to="/signup">Sign Up</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/login">LogIn</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/logout">LogOut</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import App from './components/App.js'
+import Home from './components/Home.js'
+import SignUp from './components/SignUp.js'
+import LogIn from './components/LogIn.js'
+import Dashboard from './components/Dashboard.js'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  <BrowserRouter>
+    <App>
+      <Route path="/" exact component={Home} />
+      <Route path="/signup" exact component={SignUp} />
+      <Route path="/login" exact component={LogIn} />
+      <Route path="/dashboard" exact component={Dashboard} />
+    </App>
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
